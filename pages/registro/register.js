19484cef5423199213af0c2f9ab42c6f5795e641
Matheus <matheussousa851@gmail.com>
2024-03-05 22:48:23 -0300

    register page refatorado

--- pages/registro/register.js
+++ pages/registro/register.js
@@ -1,60 +1,65 @@
 function onChangeEmail() {
     const email = form.email().value
-    form.emailRequire().style.display = email ? "none" : "block"
+    form.emailRequiredError().style.display = email ? "none" : "block"
 
-    form.emailInvalido().style.display = validateEmail(email) ? "none" : "block"
-    toggleRegisterButton()
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block"
+    toggleRegisterButtonDisable()
+ }
+
+function onChangePassword() {
+    const password = form.password().value
+
+    form.passwordRequiredError().style.display = password ? "none" : "block"
+    form.passwordMinLengthError().style.display = password.length >=6 ? "none" : "block"
+    validatePasswordMatch()
+    toggleRegisterButtonDisable() 
 }
-
-function onChangePassword(){
-    const password = form.senha().value
-    form.passwordRequire().style.display = password ? "none" : "block"
-    form.passwordMinLenght().style.display = password.length >=6 ? "none" : "block"
-
-    validatePasswordsMath()
-    toggleRegisterButton()
-}
+ 
 
 function onChangeConfirmPassword() {
-    validatePasswordsMath()
-    toggleRegisterButton()
+    validatePasswordMatch()
+    toggleRegisterButtonDisable() 
 }
 
-function validatePasswordsMath() {
-    const password = form.senha().value
-    const confirmPassword = form.confirmarSenha().value
+
+function validatePasswordMatch() {
+    const password = form.password().value
+    const confirmPassword = form.confirmPassword().value
 
     form.confirmPasswordDoesntMathError().style.display = password == confirmPassword ? "none" : "block"
 }
 
-function toggleRegisterButton() {
-    form.registerButton().disable = !isFormValid()
+function toggleRegisterButtonDisable() {
+    form.registerButton().disabled = !isFormValid()
 }
 
 function isFormValid() {
     const email = form.email().value
-    if(!email || !validateEmail(email)){
+    if (!email || !validateEmail(email)) {
         return false
     }
-    const password = form.senha().value
-    if(!password || password.length < 6){
+    const password = form.password().value
+    if (!password || password.length < 6) {
         return false
     }
-    const confirmPassword = form.confirmarSenha().value
-    if(password != confirmPassword){
+    const confirmPassword = form.confirmPassword().value
+    if (password != confirmPassword) {
         return false
     }
 
     return true
 }
+
 const form = {
-    confirmarSenha: () => document.getElementById("confirmPassword"),
-    confirmPasswordDoesntMathError: () => document.getElementById("senha-nao-igual"),
+    confirmPassword: () =>document.getElementById("confirmPassword"),
+    confirmPasswordDoesntMathError: () => document.getElementById("password-doesnt-match-error"),
     email: () => document.getElementById("email"),
-    emailRequire: () => document.getElementById("emailerror"),
-    emailInvalido: () => document.getElementById("email-invalido"),
-    senha: () => document.getElementById("senha"),
-    passwordRequire: () => document.getElementById("senha-error"),
-    passwordMinLenght: () => document.getElementById("senha-minima"),
-    registerButton: () => document.getElementById("register-button"),
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"),
+
+    password: () => document.getElementById("password"),
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    passwordMinLengthError: () => document.getElementById("password-min-length-error"),
+
+    registerButton: () => document.getElementById("register-button")
 }
